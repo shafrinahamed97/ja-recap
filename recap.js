2f5f4d0a5475abe1ee2167097064d7ca1378a485
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-24 13:01:59 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,9 @@
+/*
+  1. how to use js with html and css
+  2. get element or elements from dom
+   2-1. getElementById
+   2-2: getElementsByClassName
+   2-3: querySelector
+   2- 4: querySelectorAll
+   
+*/
